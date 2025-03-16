b2ad9714403a4ac74c89ac9653873db1cae07a10
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-03-16 21:45:46 +0530

    change the ipynb file

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
 app = Flask(__name__)
-# app.config['UPLOAD_IMAGE_FOLDER'] = 'store/images'
-# app.config['UPLOAD_AUDIO_FOLDER'] = 'store/audios'
-# app.config['UPLOAD_CV_FOLDER'] = 'store/cvs'
+app.config['UPLOAD_IMAGE_FOLDER'] = 'store/images'
+app.config['UPLOAD_AUDIO_FOLDER'] = 'store/audios'
+app.config['UPLOAD_CV_FOLDER'] = 'store/cvs'
 CORS(app)
 
 # Ensure upload folders exist
@@ -51,102 +51,102 @@ def api_face_detection():
             mimetype="application/json"
         )
 
-# @app.route('/api/face_monitoring', methods=['POST'])
-# def api_face_monitoring():
-#     try:
-#         username = request.form['username']
-#         response = face_analysis(username)
-#         return Response(
-#             response=json.dumps(response),
-#             status=200,
-#             mimetype="application/json"
-#         )
+@app.route('/api/face_monitoring', methods=['POST'])
+def api_face_monitoring():
+    try:
+        username = request.form['username']
+        response = face_analysis(username)
+        return Response(
+            response=json.dumps(response),
+            status=200,
+            mimetype="application/json"
+        )
 
-#     except Exception as e:
-#         return Response(
-#             response=json.dumps({
-#                 "message": "Face monitoring failed",
-#                 "error": str(e)
-#             }),
-#             status=400,
-#             mimetype="application/json"
-#         )
+    except Exception as e:
+        return Response(
+            response=json.dumps({
+                "message": "Face monitoring failed",
+                "error": str(e)
+            }),
+            status=400,
+            mimetype="application/json"
+        )
 
-#     try:
-#         audio_file = request.files['audio_file']
-#         save_path = os.path.join(app.config['UPLOAD_AUDIO_FOLDER'], secure_filename(audio_file.filename))
-#         audio_file.save(save_path)
+    try:
+        audio_file = request.files['audio_file']
+        save_path = os.path.join(app.config['UPLOAD_AUDIO_FOLDER'], secure_filename(audio_file.filename))
+        audio_file.save(save_path)
 
-#         response = flowAnalyzerPipeline(save_path)
+        response = flowAnalyzerPipeline(save_path)
 
-#         return Response(
-#             response=json.dumps(response),
-#             status=200,
-#             mimetype="application/json"
-#         )
-#     except Exception as e:
-#         return Response(
-#             response=json.dumps({
-#                 "message": "Flow analysis failed",
-#                 "error": str(e)
-#             }),
-#             status=400,
-#             mimetype="application/json"
-#         )
+        return Response(
+            response=json.dumps(response),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as e:
+        return Response(
+            response=json.dumps({
+                "message": "Flow analysis failed",
+                "error": str(e)
+            }),
+            status=400,
+            mimetype="application/json"
+        )
 
-# @app.route('/api/answer_evaluation', methods=['POST'])
-# def api_answer_evaluation():
-#     try:
-#         question = request.form['question']
-#         correct_answer = request.form['correct_answer']
-#         user_answer = request.form['user_answer']
+@app.route('/api/answer_evaluation', methods=['POST'])
+def api_answer_evaluation():
+    try:
+        question = request.form['question']
+        correct_answer = request.form['correct_answer']
+        user_answer = request.form['user_answer']
 
-#         response = inference_answer_evaluation(question, correct_answer, user_answer)
+        response = inference_answer_evaluation(question, correct_answer, user_answer)
 
-#         return Response(
-#             response=json.dumps({
-#                 "Score": response
-#             }),
-#             status=200,
-#             mimetype="application/json"
-#         )
+        return Response(
+            response=json.dumps({
+                "Score": response
+            }),
+            status=200,
+            mimetype="application/json"
+        )
 
-#     except Exception as e:
-#         return Response(
-#             response=json.dumps({
-#                 "message": "Answer evaluation failed",
-#                 "error": str(e)
-#             }),
-#             status=400,
-#             mimetype="application/json"
-#         )
+    except Exception as e:
+        return Response(
+            response=json.dumps({
+                "message": "Answer evaluation failed",
+                "error": str(e)
+            }),
+            status=400,
+            mimetype="application/json"
+        )
 
-# @app.route('/api/document_rag', methods=['POST'])
-# def api_document_rag():
-#     try:
-#         cv = request.files['cv']
-#         cv_path = os.path.join(app.config['UPLOAD_CV_FOLDER'], secure_filename(cv.filename))
-#         cv.save(cv_path)
+@app.route('/api/document_rag', methods=['POST'])
+def api_document_rag():
+    try:
+        cv = request.files['cv']
+        cv_path = os.path.join(app.config['UPLOAD_CV_FOLDER'], secure_filename(cv.filename))
+        cv.save(cv_path)
 
-#         response = retrieve_documents(cv_path)
-#         response = json.loads(response)
-#         return Response(
-#             response=json.dumps({
-#                 "JDs": response
-#             }),
-#             status=200,
-#             mimetype="application/json"
-#         )
+        response = retrieve_documents(cv_path)
+        response = json.loads(response)
+        return Response(
+            response=json.dumps({
+                "JDs": response
+            }),
+            status=200,
+            mimetype="application/json"
+        )
 
-#     except Exception as e:
-#         return Response(
-#             response=json.dumps({
-#                 "message": "Document RAG failed",
-#                 "error": str(e)
-#             }),
-#             status=400,
-#             mimetype="application/json"
-#         )
+    except Exception as e:
+        return Response(
+            response=json.dumps({
+                "message": "Document RAG failed",
+                "error": str(e)
+            }),
+            status=400,
+            mimetype="application/json"
+        )
 
 if __name__ == '__main__':
     app.run(
